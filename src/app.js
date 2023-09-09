8ef85d725581ac3588f4234c450935fe0af4556a
Daniel Quintero <daqcarrillo@gmail.com>
2023-09-09 19:27:35 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const server = express();
 server.name = 'API';
 
 const corsOptions = {
-  origin:'http://localhost:3001',
+  origin:'http://localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
